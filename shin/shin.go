cb4a0f6ce990eca51190ad5edd2d934d9277c52d
Michael Penkov <m@penkov.dev>
2022-04-28 15:59:22 +0900

    more work on shin

--- shin/shin.go
+++ shin/shin.go
@@ -1,17 +1,21 @@
 //
 // SSH into an AWS EC2 instance by its instance name
 //
+// Use the IDENTITY_FILE_PATH environment variable to specify the *.pem file to connect with.
+//
 // TODO:
 //
 // - [ ] handle different AWS config profiles
 // - [ ] handle EC2 instance tag names
-// - [ ] update local ~/.ssh/config with instance info so you can just directly SSH into it
-// - [ ] parameterize username (e.g. don't hardcode "ubuntu")
+// - [x] update local ~/.ssh/config with instance info so you can just directly SSH into it
+// - [x] parameterize username (e.g. don't hardcode "ubuntu")
 //
 package main
 
 import (
+	"bytes"
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -19,9 +23,10 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
-func ipAddressFromIID(instanceId string) string {
+func findInstance(instanceId string) (ec2types.Instance, error) {
 	ctx := context.TODO()
 	config, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
@@ -31,18 +36,49 @@ func ipAddressFromIID(instanceId string) string {
 	params := ec2.DescribeInstancesInput{InstanceIds: []string{instanceId}}
 	output, err := client.DescribeInstances(ctx, &params)
 	if err != nil {
-		log.Fatal(err)
+		return ec2types.Instance{}, err
 	}
-	for _, reservation := range output.Reservations {
-		for _, instance := range reservation.Instances {
-			return *instance.PublicIpAddress
-		}
-	}
-	return ""
+	return output.Reservations[0].Instances[0], nil
 }
 
-func ssh(ip string) error {
-	userAtHost := fmt.Sprintf("ubuntu@%s", ip)
+func register(instance ec2types.Instance, alias, username string) error {
+	defaultAlias := *instance.InstanceId
+	for _, tag := range instance.Tags {
+		if *tag.Key == "Name" {
+			defaultAlias = *tag.Value
+			break
+		}
+	}
+	if alias == "" {
+		alias = defaultAlias
+	}
+
+	identityFilePath := os.ExpandEnv("$IDENTITY_FILE_PATH")
+
+	var buf bytes.Buffer
+	buf.WriteString("\n# <shin>\n")
+	buf.WriteString(fmt.Sprintf("Host %s\n", alias))
+	buf.WriteString(fmt.Sprintf("\tHostname %s\n", *instance.PublicIpAddress))
+	buf.WriteString(fmt.Sprintf("\tUser %s\n", username))
+	buf.WriteString(fmt.Sprintf("\tIdentityFile %s\n", identityFilePath))
+	buf.WriteString("# </shin>\n")
+
+	fmt.Fprintf(os.Stderr, buf.String())
+
+	path := os.ExpandEnv("$HOME/.ssh/config")
+	fout, err := os.OpenFile(path, os.O_APPEND | os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer fout.Close()
+
+	_, err = fout.WriteString(buf.String())
+	return err
+
+}
+
+func ssh(ip string, username string) error {
+	userAtHost := fmt.Sprintf("%s@%s", username, ip)
 	identityFilePath := os.ExpandEnv("$IDENTITY_FILE_PATH")
 	command := exec.Command(
 		"ssh",
@@ -60,9 +96,34 @@ func ssh(ip string) error {
 }
 
 func main() {
-	ip := ipAddressFromIID(os.Args[1])
-	err := ssh(ip)
+	var doNotConnect bool
+	var registerHost bool
+	var registerAlias string
+	var username string
+
+	//
+	// NB. When calling this program, the non-flag arguments MUST follow the flag arguments.
+	// If we don't do this, the command-line argument parsing will not work correctly.
+	//
+	flag.BoolVar(&doNotConnect, "dnc", false, "do not actually connect to the instance")
+	flag.BoolVar(&registerHost, "register", false, "register the instance instance in ~/.ssh/config")
+	flag.StringVar(&registerAlias, "alias", "", "override the alias to register")
+	flag.StringVar(&username, "username", "ubuntu", "the username to use for the connection")
+	flag.Parse()
+
+	instance, err := findInstance(flag.Arg(0))
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if registerHost {
+		register(instance, registerAlias, username)
+	}
+
+	if !doNotConnect {
+		err := ssh(*instance.PublicIpAddress, username)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
