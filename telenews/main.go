3357b48ffa68a434ab058803d6bffa9d58ba210c
Michael Penkov <m@penkov.dev>
2022-09-07 05:16:30 +0900

    implemented handling for image files

--- telenews/main.go
+++ telenews/main.go
@@ -6,7 +6,7 @@
 // - [x] Output HTML instead of text
 // - [ ] Use proper Golang templates when outputting HTML
 // - [x] Extract headlines (first line in the message) and mark them up with CSS
-// - [ ] Output media files when available
+// - [.] Output media files when available (photos, caching, etc)
 // - [ ] Configuration file (contain phone number, secrets, channel IDs, etc) - avoid signing up to public channels
 // - [ ] Show channel thumbnails
 // - [ ] Markup for hyperlinks, etc. - why isn't this already in the message text?
@@ -33,6 +33,7 @@ import (
 	"github.com/gotd/td/examples"
 	"github.com/gotd/td/telegram"
 	"github.com/gotd/td/telegram/auth"
+	"github.com/gotd/td/telegram/downloader"
 	"github.com/gotd/td/tg"
 )
 
@@ -43,6 +44,7 @@ type Item struct {
 	Text         string
 	Date         time.Time
 	Webpage      *tg.WebPage
+	Images       []string
 }
 
 func output(items []Item) {
@@ -51,7 +53,7 @@ func output(items []Item) {
 	fmt.Println("<head><style>")
 	fmt.Println("body { font-family: Helvetica; }")
 	fmt.Println(".item-list { display: grid; grid-gap: 15px; }")
-	fmt.Println(".item { display: grid; grid-template-columns: 100px 200px 800px; border-top: 1px solid gray}")
+	fmt.Println(".item { display: grid; grid-template-columns: 100px 200px 800px 300px; border-top: 1px solid gray}")
 	fmt.Println("p:nth-child(1) { font-weight: bold; font-size: large }")
 	fmt.Println(".channel { font-size: large; font-weight: bold }")
 	fmt.Println(".datestamp { font-size: xx-large; font-weight: bold; color: gray }")
@@ -78,6 +80,14 @@ func output(items []Item) {
 			fmt.Println("</blockquote>")
 		}
 		fmt.Println("</span>") // message
+
+		if len(item.Images) > 0 {
+			fmt.Println("<span class='images'>")
+			for _, img := range item.Images {
+				fmt.Println(fmt.Sprintf("<img src=%q></img>", img))
+			}
+			fmt.Println("</span>")
+		}
 		fmt.Println("</span>") // item
 	}
 	fmt.Println("</div></body></html>")
@@ -148,7 +158,6 @@ func decodeChannel(inputPeerClass tg.InputPeerClass) (channel tg.InputChannel, e
 }
 
 func decodeMessages(log *zap.Logger, mmc tg.MessagesMessagesClass) (messages []tg.Message, err error) {
-	log.Debug(fmt.Sprintf("mmc.TypeName = %s", mmc.TypeName()))
 	var innerMessages []tg.MessageClass
 
 	switch inner := mmc.(type) {
@@ -161,7 +170,6 @@ func decodeMessages(log *zap.Logger, mmc tg.MessagesMessagesClass) (messages []t
 	}
 
 	for _, m := range innerMessages {
-		log.Debug(fmt.Sprintf("m.TypeName = %s", m.TypeName()))
 		log.Debug(m.String())
 		switch message := m.(type) {
 		case *tg.Message:
@@ -191,7 +199,40 @@ func getChannelInfo(input tg.InputChannel, client *tg.Client, ctx context.Contex
 	return "", "", fmt.Errorf("not implemented yet")
 }
 
+func extractThumbnailLocation(log *zap.Logger, photo tg.Photo) tg.InputPhotoFileLocation {
+	log.Debug(photo.String())
+	// TODO: do something with this thing
+	// https://core.telegram.org/api/files#downloading-files
+	// Where do we store these files?  In a local dir, or embedded into the HTML?
+	var sizes []tg.PhotoSize
+	for _, photoSizeClass := range photo.Sizes {
+		switch photoSize := photoSizeClass.(type) {
+		case *tg.PhotoSize:
+			sizes = append(sizes, *photoSize)
+			break
+		}
+	}
+	//
+	// We want the smallest possible image, for now
+	//
+	sort.Slice(sizes, func(i, j int) bool { return sizes[i].Size < sizes[j].Size })
+	if len(sizes) > 0 {
+		location := tg.InputPhotoFileLocation{
+			ID:            photo.ID,
+			AccessHash:    photo.AccessHash,
+			FileReference: photo.FileReference,
+			ThumbSize:     sizes[0].Type,
+		}
+		log.Debug(fmt.Sprintf("location: %s", location.String()))
+		return location
+	}
+
+	return tg.InputPhotoFileLocation{}
+}
+
 func main() {
+	thresholdDate := time.Now().Unix() - 24*3600
+
 	phone := flag.String("phone", "", "phone number to authenticate")
 	flag.Parse()
 
@@ -262,14 +303,13 @@ func main() {
 							continue
 						}
 
-						thresholdDate := time.Now().Unix() - 24*3600
-
 						for _, m := range messages {
 							if int64(m.Date) < thresholdDate {
 								// old news from over 24 hours ago
 								continue
 							}
 							var webpage *tg.WebPage
+							var thumbnailLocation tg.InputPhotoFileLocation
 
 							if m.Media != nil {
 								log.Debug("attachment: " + m.Media.TypeName())
@@ -278,10 +318,7 @@ func main() {
 								case *tg.MessageMediaPhoto:
 									switch photo := messageMedia.Photo.(type) {
 									case *tg.Photo:
-										log.Debug(photo.String())
-										// TODO: do something with this thing
-										// https://core.telegram.org/api/files#downloading-files
-										// Where do we store these files?  In a local dir, or embedded into the HTML?
+										thumbnailLocation = extractThumbnailLocation(log, *photo)
 										break
 									}
 									break
@@ -294,6 +331,21 @@ func main() {
 									}
 								}
 							}
+
+							// TODO: configurable cache location
+							//
+							var images []string
+							if thumbnailLocation.ID != 0 {
+								path := fmt.Sprintf("/tmp/%d.jpeg", thumbnailLocation.ID)
+								_, err := os.Stat(path)
+								if err != nil {
+									dloader := downloader.NewDownloader()
+									builder := dloader.Download(client.API(), &thumbnailLocation)
+									builder.ToPath(ctx, path)
+								}
+								images = append(images, path)
+							}
+
 							// https://stackoverflow.com/questions/24987131/how-to-parse-unix-timestamp-to-time-time
 							tm := time.Unix(int64(m.Date), 0)
 							item := Item{
@@ -303,9 +355,11 @@ func main() {
 								Text:         m.Message,
 								Date:         tm,
 								Webpage:      webpage,
+								Images:       images,
 							}
 							items = append(items, item)
-							log.Info(fmt.Sprintf("handled MessageID %d (%s) from Domain %s (%d char)", item.MessageID, item.Date, item.Domain, len(item.Text)))
+
+							log.Info(fmt.Sprintf("handled MessageID %d (%s) from Domain %s (%d char, %d images)", item.MessageID, item.Date, item.Domain, len(item.Text), len(item.Images)))
 						}
 					}
 				}
